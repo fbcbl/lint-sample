76dceebc0fb0b0fbafb98b76089fc38a46275e26
Fábio Carballo <fabio.og.carballo@gmail.com>
2020-01-02 14:26:39 +0100

    add api level

--- rules/src/main/java/com/fabiocarballo/rules/IssueRegistry.kt
+++ rules/src/main/java/com/fabiocarballo/rules/IssueRegistry.kt
@@ -1,10 +1,13 @@
 package com.fabiocarballo.rules
 
 import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
 
 class IssueRegistry : IssueRegistry() {
 
+    override val api: Int = CURRENT_API
+    
     override val issues: List<Issue>
         get() = listOf(AndroidLogDetector.ISSUE)
 }
