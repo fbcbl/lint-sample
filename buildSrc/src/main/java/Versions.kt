50bb613cb603205a93a04801a002e29f332d5b95
Fábio Carballo <fabio.og.carballo@gmail.com>
2020-01-02 09:34:21 +0100

    add buildSrc and proper dependency management

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,8 @@
+object Versions {
+
+    val androidAppCompat = "1.0.2"
+    val androidGradlePlugin = "3.5.3"
+    val androidKtxCore = "1.0.2"
+    val kotlin = "1.3.61"
+    val lint = "26.5.3"
+}
